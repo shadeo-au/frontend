b4eee03b2446eb8e69fc44efbccd24ec0dc23d29
Teng Luo <tluo0021@student.monash.edu>
2026-05-04 20:16:47 +1000

    Add password

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,11 @@ declare module '*.vue' {
   const component: DefineComponent<object, object, unknown>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_GATE_HASH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
