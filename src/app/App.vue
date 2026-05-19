75c9a5d632590fee8f24cefa3b4defb448128af8
Teng Luo <tluo0021@student.monash.edu>
2026-05-19 13:46:59 +1000

    delete access password

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,25 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import { RouterView } from 'vue-router';
-import PasswordGate from '../components/PasswordGate.vue';
-
-const SESSION_KEY = 'shadeo_gate_unlocked_v1';
-const unlocked = ref(readSessionFlag());
-
-function readSessionFlag(): boolean {
-  try {
-    return sessionStorage.getItem(SESSION_KEY) === '1';
-  } catch {
-    return false;
-  }
-}
-
-function onUnlock() {
-  unlocked.value = true;
-}
 </script>
 
 <template>
-  <RouterView v-if="unlocked" />
-  <PasswordGate v-else @unlock="onUnlock" />
+  <RouterView />
 </template>
